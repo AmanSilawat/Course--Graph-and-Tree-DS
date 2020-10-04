3d30a777fe4f70dc38cc91adf849be06682b2867
aman <aman.khan.silawat.aks@gmail.com>
2020-10-04 11:48:46 +0530

    Added Recommendation

--- binary_tree_traversal.js
+++ binary_tree_traversal.js
@@ -32,22 +32,32 @@ class BinaryTree {
     }
 
     traverse(func) {
-        func(tree.question);
+        func(this.question);
         if (this.yes) this.yes.traverse(func);
         if (this.no) this.no.traverse(func);
+    }
+
+    countReccos(node) {
+        if (node === null) {return 0;}
+        if (!node.yes && !node.no) {
+            return 1;
+        }
+
+        return (countReccos(node.yes)) +
+        (countReccos(node.no));
+
     }
 
     contains(question) {
         if (this.question === question) {
             return true;
         }
-        return (
-            (this.yes && this.yes.contains(question) ? true : false) ||
+        return (this.yes && this.yes.contains(question) ? true : false) ||
             (this.no && this.no.contains(question) ? true : false)
-        );
+        ;
     }
 }
-
-export default BinaryTree;
-
+let myTree = new BinaryTree('You like Cricket');
+console.log(myTree);
+// myTree.traverse('Does cricket have 11 players per side. ', 'yes');
 myTree.traverse(console.log);
